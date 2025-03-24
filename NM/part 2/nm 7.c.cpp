01d8e201149dbd478a80798fd83ca4de9d1a6f5e
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-03-24 14:22:59 +0545

    Simpson 1/3 rule

---
+++ NM/part 2/nm 7.c.cpp
@@ -0,0 +1,32 @@
+//7. WAP to compute the numerical integration using 
+//c. Simpson's 1/3 Rule 
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+//integrate f(x) =(x^3+2) for limit x=2 to 8
+
+float func(float x){
+//	return pow(x,3)+2;
+	return sqrt(1-x*x);
+}
+int main(){
+	float a=0,b=1;
+	int n=2;
+	cout<<"\tComposite Trapezodial rule\n";
+	cout<<"lower and upper limits:  ";
+	cin>>a>>b;
+	
+	float h=(b-a)/n;
+	float  x[3]={a,a+h,b};
+	float simpson =(func(x[0]) + 4*func(x[1])+func(x[2]))*h/3;
+	
+	
+	cout<<"Interpolation of f(x)=sqrt(1-x*x): "<<simpson;
+	
+	cout<<endl<<string(35,'_')<<endl;
+	cout<<"Name :Santosh B.K. \tSection : C\nRoll no:76"<<endl;
+	cout<<"Lab no: 7c"<<endl;
+	return 0;
+}
