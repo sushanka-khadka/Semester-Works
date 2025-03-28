f6e102f9f31260ee08ba027e30af11cb29966db6
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-03-28 11:51:51 +0545

    job sequencing with deadline

---
+++ DAA/5.cpp
@@ -0,0 +1,18 @@
+//  Job Sequencing with deadline(Greedy Algorithm)
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int n, arr[20];
+	cout<<"---------Job Sequencing with deadline(Greedy Algorithm)---------"<<endl;
+	cout<<"Enter size of array : ";
+	cin>>n;
+	cout<<"Enter array: ";
+	for(int i=0; i<n;i++){
+		cin>>arr[i];
+	}	
+
+	return 0;
+}
