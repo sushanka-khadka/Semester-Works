ab4c48ae44ab7c37379bbc8b42bc3e3a26b4f8d4
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-19 09:03:26 +0545

    initial setup

---
+++ 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/Data/EF_Core_DbContext.cs
@@ -0,0 +1,12 @@
+﻿using EF_Core_DB_First.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core_DB_First.Data
+{
+    public class EF_Core_DbContext(DbContextOptions<EF_Core_DbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+        public DbSet <Child> Children { get; set; }
+    }
+}
---
+++ 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EF_Core_DB_First.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
