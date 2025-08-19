531a5c17036b51a4219f90b39500edfffe634488
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-19 09:03:26 +0545

    initial setup

---
+++ 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/Models/Child.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF_Core_DB_First.Models
+{
+    public class Child
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [RegularExpression(@"Male|Female", ErrorMessage = "Gender is either Male or Female.")]
+        public string Gender { get; set; }
+        // no foregin key in main table as it is stored in join table
+        // navigation to parent
+        public ICollection<Parent> Parents { get; set; }
+    }
+}
---
+++ 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/Models/Parent.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Core_DB_First.Models
+{
+    public class Parent
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int hasChildren { get; set; }
+
+        // foreign key to employee
+        [ForeignKey("Employee")]
+        public int EmpId { get; set; }
+        // navigation property
+        public Employee Employee { get; set; }
+        [Required]
+
+        // navigation property for children (many - many)
+        public ICollection<Child> Children { get; set; }
+    }
+}
