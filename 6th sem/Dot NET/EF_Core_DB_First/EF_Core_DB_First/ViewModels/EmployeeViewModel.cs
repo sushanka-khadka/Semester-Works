35f343dbec2a517dc5621491bdb973b4abff5245
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-20 22:49:08 +0545

    Adding ViewModel to Employee

---
+++ 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EF_Core_DB_First.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        [Key]
+        public int EmpId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FullName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; }
+        [Required]
+        [StringLength(maximumLength:10, MinimumLength =10, ErrorMessage ="Phone number must be 10 digits.")]
+        [RegularExpression(@"(98|97)\d{8}$", ErrorMessage ="Enter a valid Nepali Phone number.")]
+        public string Phone { get; set; }
+        [Required]
+        public bool IsMarried { get; set; }
+        [Required]
+        [RegularExpression(@"Male|Female", ErrorMessage = "Gender is either Male or Female.")]
+        public string Gender { get; set; }
+    }
+}
---
+++ 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/ViewModels/ParentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Core_DB_First.ViewModels
+{
+    public class ParentViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int HasChildren { get; set; }
+
+        // foreign key to employee
+        [ForeignKey("Employee")]
+        public int EmpId { get; set; }
+        
+        // no navigation property as this is ViewModel
+    }
+}
