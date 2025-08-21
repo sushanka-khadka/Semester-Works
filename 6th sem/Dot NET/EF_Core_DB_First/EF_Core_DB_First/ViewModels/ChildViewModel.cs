ce4b7c26bc820a52f9e8d514ea4dd387291c680f
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-21 10:43:21 +0545

    adding property for selected parents

--- 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/ViewModels/ChildViewModel.cs
+++ 6th sem/Dot NET/EF_Core_DB_First/EF_Core_DB_First/ViewModels/ChildViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace EF_Core_DB_First.ViewModels
 {
@@ -11,5 +12,10 @@ namespace EF_Core_DB_First.ViewModels
         public string Name { get; set; }
         [RegularExpression(@"Male|Female", ErrorMessage = "Gender is either Male or Female.")]
         public string Gender { get; set; }
+
+        // selected parentIds from dropdown/multiselect
+        public List<int> SelectedParentIds { get; set; }
+        // selected list items
+        public List<SelectListItem> AvailableParents { get; set; }
     }
 }
