5f7035aac7599a1602a0257002bee5e488709150
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-09 08:20:23 +0545

    login & logout feature

--- 6th sem/Ecommerce/ecom/store/urls.py
+++ 6th sem/Ecommerce/ecom/store/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('register', views.register_user, name='register'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 6th sem/Ecommerce/ecom/store/views.py
+++ 6th sem/Ecommerce/ecom/store/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 # Create your views here.
-def index(request):
-    return render(request, 'home.html')
+def home(request):
+    products= Product.objects.all()
+    return render(request, 'home.html', {
+        'products': products
+    })
+
+def about(request):
+    return render(request, 'about.html')
+
+def login_user(request):
+    if (request.method == 'POST'):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:    # Check if authentication was successful - if user exists
+            login(request, user)
+            messages.success(request, "Logged in successfully!")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect('login')        
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request=request)
+    messages.success(request=request, message="Logout Successfully!!!")
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html')
