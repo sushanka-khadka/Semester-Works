d86327dea3da9339d907d08566e3c9364ea444a4
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-10 22:17:17 +0545

    A context processor that makes the cart available in every template

---
+++ 6th sem/Ecommerce/ecom/cart/context_processors.py
@@ -0,0 +1,7 @@
+from cart.cart import Cart
+
+
+# create context processor class so the cart can work on all pages of site
+def cart(request):
+    # return the default data from the cart
+    return { 'Cart': Cart(request) }
