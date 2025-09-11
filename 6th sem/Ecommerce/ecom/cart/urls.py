94405a41d7e6917499ec7232bb1ed21766236bff
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-11 16:19:32 +0545

    storing as session

--- 6th sem/Ecommerce/ecom/cart/urls.py
+++ 6th sem/Ecommerce/ecom/cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.cart_summary, name='cart-summary'),
-    path('add/', views.cart_add),
+    path('add/', views.cart_add, name='cart-add'),
     path('delete/', views.cart_delete),
     path('update/', views.cart_update)
 ]
--- 6th sem/Ecommerce/ecom/cart/views.py
+++ 6th sem/Ecommerce/ecom/cart/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 def cart_summary(request):
-    return render(request, 'cart_summary.html')
+    cart = Cart(request)    # get cart object
+    cart_products = cart.get_products()
+    
+    return render(request, 'cart_summary.html', {
+        'cart_products': cart_products
+    })
 
 def cart_add(request):
-    return None
+    cart = Cart(request)
+    # json response
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        # lookup products in DB
+        product = get_object_or_404(Product, id=product_id) #  Fetches the corresponding product object from the database.
+        # save to the session
+        cart.add(product= product)
+        # return response
+        response = JsonResponse({'Product name':product.name})
+        return response
+    
 
 def cart_delete(request):
     return None
