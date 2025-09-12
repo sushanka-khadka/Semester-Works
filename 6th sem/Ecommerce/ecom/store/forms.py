b29460ff56a62b2f7252bbde0d9208ab133556c6
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-12 08:24:49 +0545

    update user profile

--- 6th sem/Ecommerce/ecom/store/forms.py
+++ 6th sem/Ecommerce/ecom/store/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 
 class SignUpForm(UserCreationForm):
@@ -34,5 +34,22 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].help_text = '<span class="form-text text-muted small">Enter the same password as before, for verification.</small></span>' 
 
 
+class UpdateUserForm(UserChangeForm):
+    # hides password 
+    password = None     # else show suggest for password change
+    # get other fields
+    email = forms.EmailField(label='', widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    first_name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name' }))
+    last_name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Last Name' }))
 
-        
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and  digits and @/./+/-/_ only.</small></span>'        
