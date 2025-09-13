cce57b8c6c1cc6c6b4c6cab441ee46e823687946
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-14 01:01:00 +0545

    bill checkout  setup

--- 6th sem/Ecommerce/ecom/payment/urls.py
+++ 6th sem/Ecommerce/ecom/payment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('payment_sucess', views.payment_success, name='payment-success')
+    path('payment_sucess', views.payment_success, name='payment-success'),
+    path('checkout', views.checkout, name='checkout')
 ]
