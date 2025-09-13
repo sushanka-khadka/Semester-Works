02171b58e245ed47c70940fa6add5a3e0275d9d6
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-14 00:06:56 +0545

    Order & OrderItem model

--- 6th sem/Ecommerce/ecom/payment/models.py
+++ 6th sem/Ecommerce/ecom/payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 # Create your models here.
 # --------- Payment and Shipping
@@ -20,3 +21,26 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
+    
+
+# create Order Model
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    shipping_address = models.CharField(max_length=200)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)  # never updates (one time set)
+
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f'Order Item - {str(self.id)}'
