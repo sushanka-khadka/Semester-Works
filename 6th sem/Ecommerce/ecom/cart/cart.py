746a3cb45010afd0a6d49d9dbcbe8675787321e7
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-13 20:34:02 +0545

    login cart persistence

--- 6th sem/Ecommerce/ecom/cart/cart.py
+++ 6th sem/Ecommerce/ecom/cart/cart.py
@@ -1,8 +1,12 @@
-from store.models import Product
+from store.models import Product, Profile
+import json
+from django.contrib.sessions.models import Session
 
 class Cart():
     def __init__(self, request):    #  Connects the cart to the user’s request.session.
         self.session = request.session      #  attach the user's session to this cart object
+        # get request
+        self.request = request
 
         # get the current session key if it exists
         cart = self.session.get('session_key')
@@ -26,6 +30,42 @@ class Cart():
 
         self.session.modified =  True
     
+        # Deal with logged in user
+        if self.request.user.is_authenticated:
+            # get the current user profile
+            current_user = Profile.objects.filter(user__id = self.request.user.id)
+            # Convert {'3':2, '2':5} to {"3":2, "2":5}  =>  single quote ->> double quote 
+            # carty = str(self.cart).replace("\'", "\"")  (risky)
+            carty = json.dumps(self.cart)   
+            '''
+            # JSON property name enclosed in double quotes
+            # bcz we need to convert this session string (cart) into json later to loop througn items 
+            '''
+            
+            # save carty to the Profile Model
+            current_user.update(old_cart= str(carty))
+
+    
+    def db_add(self, product_id, quantity):
+        product_id = str(product_id)
+        product_qty = str(quantity)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = int(product_qty)
+        
+        self.session.modified =  True
+    
+        # Deal with logged in user
+        if self.request.user.is_authenticated:
+            # get the current user profile
+            current_user = Profile.objects.filter(user__id = self.request.user.id)
+            carty = json.dumps(self.cart)
+
+            # save carty to the Profile Model
+            current_user.update(old_cart= str(carty))
+
 
     def cart_total(self):
         product_ids = self.cart.keys()  # collect the product ids from the cart
@@ -67,6 +107,13 @@ class Cart():
 
         self.cart[product_id] = product_qty
         self.session.modified = True
+
+        if self.request.user.is_authenticated:            
+            current_user = Profile.objects.filter(user__id = self.request.user.id)
+            carty = json.dumps(self.cart)
+
+            # save carty to the Profile Model
+            current_user.update(old_cart= str(carty))
         
         
     def delete(self, product):
@@ -81,6 +128,12 @@ class Cart():
             del self.cart[product_id]
         
         self.session.modified = True
-        # return self.cart
+        
+        if self.request.user.is_authenticated:            
+            current_user = Profile.objects.filter(user__id = self.request.user.id)
+            carty = json.dumps(self.cart)
+
+            # save carty to the Profile Model
+            current_user.update(old_cart= str(carty))
 
 
--- 6th sem/Ecommerce/ecom/store/models.py
+++ 6th sem/Ecommerce/ecom/store/models.py
@@ -62,6 +62,8 @@ class Profile(models.Model):
     state = models.CharField(max_length=100, blank=True)
     zipcode = models.CharField(max_length=100, blank=True)
     country = models.CharField(max_length=100, blank=True)
+    
+    old_cart = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
