8d2cb39636f2b4294bc98614fb5f3bd3eb0f9091
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-13 07:27:40 +0545

    user profile model

--- 6th sem/Ecommerce/ecom/store/models.py
+++ 6th sem/Ecommerce/ecom/store/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Category(models.Model):
@@ -45,3 +49,29 @@ class Order(models.Model):
 
     def __str__(self):
         return self.product.name
+
+
+# ----- Extend user profile ----------#
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=10, validators=[RegexValidator(regex=r'^9[8,7]\d{8}$', message="Please enter valid nepali phone number.")])
+    address1= models.CharField(max_length=200)
+    address2= models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    zipcode = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    # create user profile by default when user signs up 
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            user_profile = Profile(user=instance)
+            user_profile.save()
+    
+    # automate the profile (triggered every time a model instance is saved — either created (.create() / .save()) or updated.)
+    post_save.connect(create_profile, sender=user)
+    
