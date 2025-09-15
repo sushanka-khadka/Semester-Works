958ba13d947c41bca008556ba578f05c0fff8428
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-15 17:32:11 +0545

    Inline OrderItem model

--- 6th sem/Ecommerce/ecom/payment/admin.py
+++ 6th sem/Ecommerce/ecom/payment/admin.py
@@ -5,3 +5,20 @@ from .models import ShippingAddress, Order, OrderItem
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+# OrderItem Inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0   # no empty forms by default
+
+# Extend Order Model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ['date_ordered']
+    inlines = [OrderItemInline]
+
+# unregister old Order Model
+admin.site.unregister(Order)
+
+# reregister Order and OrderItem
+admin.site.register(Order, OrderAdmin)
