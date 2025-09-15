9ba7b3facd7cdca050f139c7b637949904e899c5
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-09-15 21:39:48 +0545

    shipping dashboard

--- 6th sem/Ecommerce/ecom/payment/urls.py
+++ 6th sem/Ecommerce/ecom/payment/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('payment_sucess', views.payment_success, name='payment-success'),
     path('checkout', views.checkout, name='checkout'),
     path('billing_info', views.billing_info, name='billing-info'),
-    path('process_order', views.process_order, name='process-order')
+    path('process_order', views.process_order, name='process-order'),
+    path('shipped_dash', views.shipped_dash, name='shipped-dash'),
+    path('not_shipped_dash', views.not_shipped_dash, name='not-shipped-dash'),
 ]
